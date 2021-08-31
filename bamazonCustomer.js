2b38ae6538ded860fe86045338d81b0abb3fca00
Rhonda Kremer <rhondakremer@gmail.com>
2021-08-31 19:43:28 -0400

    fixes alignment

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,8 +1,9 @@
 require('dotenv').config()
 
-var inquirer = require("inquirer");
-var mysql=require("mysql2");
-var connection=mysql.createConnection({
+const inquirer = require("inquirer");
+
+const mysql = require("mysql2");
+const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USERNAME,
@@ -12,80 +13,70 @@ var connection=mysql.createConnection({
 
 function showProducts() {
     connection.connect(function(error){
-    if(error) {
-        console.log(error)
-    } else {
-        //console.log("You have successfully connected!");
-        connection.query("SELECT * FROM products", function(error, res){
-            if(error) {
-                console.log(error)
-            } else {
-
-                
-               for (let i = 0; i < res.length; i++) {
-
-                   console.log("Item ID: " + res[i].item_id + "|| Product: " + res[i].product_name + "|| Price: $" + res[i].price);
-                   console.log("-------")
-                   
-               }
-               purchasePrompt();
-            }
-        }) 
-    }
-});
+        if(error) {
+            console.log(error)
+        } 
+        else {
+            connection.query("SELECT * FROM products", function(error, res){
+                if(error) {
+                    console.log(error)
+                } else {
+                    for (let i = 0; i < res.length; i++) {
+                        console.log("Item ID: " + res[i].id + "|| Product: " + res[i].product_name + "|| Price: $" + res[i].price);
+                        console.log("-------")
+                }
+                purchasePrompt();
+                }
+            }) 
+        }
+    });
 }
-
 
 function purchasePrompt() {
     inquirer.prompt([
         {
-        type: "input",
-        name: "id",
-        message : "Pick enter the ID of the item you would like to purchase",
+            type: "input",
+            name: "id",
+            message : "Pick enter the ID of the item you would like to purchase",
         },
         {
-        type: "input",
-        name: "quantity",
-        message: "How many would you like to purchase?"
+            type: "input",
+            name: "quantity",
+            message: "How many would you like to purchase?"
         }
     ])
     .then(function(response) {
-        requestedItem = response.id;
-        requestedQuantity = parseInt(response.quantity)
+        const requestedItem = response.id;
+        const requestedQuantity = parseInt(response.quantity)
         makePurchase(requestedItem, requestedQuantity)
-    }
-    )
+    })
+}
 
 function makePurchase(requestedItem, requestedQuantity) {
-    connection.query("SELECT * FROM products where item_id=" + requestedItem, function(error, res){
-            if(error) {
-                console.log(error)
-            } else {
-                if (res.length == 0) {
-                    console.log("Please pick a valid item number!")
-                    purchasePrompt()
-                }
-               for (let i = 0; i < res.length; i++) {
-                   //console.log(res)
-                   var amountAvailable = parseInt(res[i].stock_quantity);
-                   //console.log(amountAvailable)
-                   //console.log(requestedQuantity)
-                   if (amountAvailable < requestedQuantity){
+    connection.query("SELECT * FROM products where id=" + requestedItem, function(error, res){
+        if(error) {
+            console.log(error)
+        } 
+        else {
+            if (res.length == 0) {
+                console.log("Please pick a valid item number!")
+                purchasePrompt()
+            }
+            for (let i = 0; i < res.length; i++) {
+                var amountAvailable = parseInt(res[i].stock_quantity);
+                if (amountAvailable < requestedQuantity){
                     console.log("Sorry! Insufficient quantity!")
                     purchasePrompt()
-                   } else {
-                       amountAvailable -= requestedQuantity;
-                       //console.log(amountAvailable)
-                       var cost = res[i].price * requestedQuantity;
-                       connection.query("UPDATE products SET stock_quantity = " + amountAvailable + " WHERE item_id = " + requestedItem);
-                       console.log("You have purchased " + requestedQuantity + " " + res[i].product_name + "(s) for $" + cost + "! Thank you for your purchase.")
-                       purchaseAnother();
-                   }
-               }
+                } else {
+                    amountAvailable -= requestedQuantity;
+                    var cost = res[i].price * requestedQuantity;
+                    connection.query("UPDATE products SET stock_quantity = " + amountAvailable + " WHERE id = " + requestedItem);
+                    console.log("You have purchased " + requestedQuantity + " " + res[i].product_name + "(s) for $" + cost + "! Thank you for your purchase.")
+                    purchaseAnother();
+                }
             }
-        }) 
-   
-    }
+        }
+    }) 
 }
 
 function purchaseAnother() {
@@ -98,29 +89,24 @@ function purchaseAnother() {
         }
     ])
     .then(function(response) {
-        //console.log(response.another)
         if (response.another == "yes") {
             connection.query("SELECT * FROM products", function(error, res){
                 if(error) {
                     console.log(error)
-                } else {
-    
-                   for (let i = 0; i < res.length; i++) {
-    
-                       console.log("Item ID: " + res[i].item_id + "|| Product: " + res[i].product_name + "|| Price: $" + res[i].price);
-                       console.log("-------")
-                       
-                   }
-                   purchasePrompt();
+                } 
+                else {
+                    for (let i = 0; i < res.length; i++) {
+                        console.log("Item ID: " + res[i].id + "|| Product: " + res[i].product_name + "|| Price: $" + res[i].price);
+                        console.log("-------")
+                    }
+                    purchasePrompt();
                 }
             }) 
-            
         } else {
             console.log("Have a great day and come back soon!")
             connection.end();
         }
     })
 }
-
 
 showProducts()
