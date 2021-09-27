839aaf5df4f6d2a153efb99fe55bed52818c0017
Rhonda Kremer <rhondakremer@gmail.com>
2021-09-26 23:03:06 -0400

    customer formatting

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const inquirer = require("inquirer");
 
 const mysql = require("mysql2");
+
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
@@ -13,16 +14,16 @@ const connection = mysql.createConnection({
 
 function showProducts() {
     connection.connect(function(error){
-        if(error) {
+        if (error) {
             console.log(error)
         } 
         else {
             connection.query("SELECT * FROM products", function(error, res){
-                if(error) {
+                if (error) {
                     console.log(error)
                 } else {
                     for (let i = 0; i < res.length; i++) {
-                        console.log("Item ID: " + res[i].id + "|| Product: " + res[i].product_name + "|| Price: $" + res[i].price);
+                        console.log(`Item ID: ${res[i].id} || Product: ${res[i].product_name} || Price: $${res[i].price}`)
                         console.log("-------")
                 }
                 purchasePrompt();
@@ -47,31 +48,31 @@ function purchasePrompt() {
     ])
     .then(function(response) {
         const requestedItem = response.id;
-        const requestedQuantity = parseInt(response.quantity)
-        makePurchase(requestedItem, requestedQuantity)
+        const requestedQuantity = parseInt(response.quantity);
+        makePurchase(requestedItem, requestedQuantity);
     })
 }
 
 function makePurchase(requestedItem, requestedQuantity) {
-    connection.query("SELECT * FROM products where id=" + requestedItem, function(error, res){
-        if(error) {
+    connection.query(`SELECT * FROM products where id=${requestedItem}`, function(error, res){
+        if (error) {
             console.log(error)
         } 
         else {
-            if (res.length == 0) {
-                console.log("Please pick a valid item number!")
-                purchasePrompt()
+            if (res.length === 0) {
+                console.log("Please pick a valid item number!");
+                purchasePrompt();
             }
             for (let i = 0; i < res.length; i++) {
-                var amountAvailable = parseInt(res[i].stock_quantity);
+                const amountAvailable = parseInt(res[i].stock_quantity);
                 if (amountAvailable < requestedQuantity){
-                    console.log("Sorry! Insufficient quantity!")
-                    purchasePrompt()
+                    console.log("Sorry! Insufficient quantity!");
+                    purchasePrompt();
                 } else {
-                    amountAvailable -= requestedQuantity;
-                    var cost = res[i].price * requestedQuantity;
-                    connection.query("UPDATE products SET stock_quantity = " + amountAvailable + " WHERE id = " + requestedItem);
-                    console.log("You have purchased " + requestedQuantity + " " + res[i].product_name + "(s) for $" + cost + "! Thank you for your purchase.")
+                    const newAvailable = amountAvailable - requestedQuantity;
+                    const cost = res[i].price * requestedQuantity;
+                    connection.query(`UPDATE products SET stock_quantity = ${newAvailable} WHERE id = ${requestedItem}`);
+                    console.log(`You have purchased ${requestedQuantity} ${res[i].product_name}(s) for $${cost}! Thank you for your purchase.`);
                     purchaseAnother();
                 }
             }
@@ -89,7 +90,7 @@ function purchaseAnother() {
         }
     ])
     .then(function(response) {
-        if (response.another == "yes") {
+        if (response.another === "yes") {
             connection.query("SELECT * FROM products", function(error, res){
                 if(error) {
                     console.log(error)
